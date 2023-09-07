63ba13f815535fb5f7ccdfed2d527db0564a4667
Yousef <yousefsameh24@gmail.com>
2023-09-07 21:40:00 +0300

    create user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true, minlength: 3 },
+    passwordHash: { type: String, required: true },
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    liked_posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    liked_comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    profile_pic: { type: String, default: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1595347704/default_profile_pic.png' },
+    bio: { type: String, default: '' },
+    dob: { type: Date, default: Date.now }
+}, { timestamps: true });
+
+userSchema.virtual('url').get(function () {
+    return `/${this.username}`;
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+        delete ret.passwordHash;
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
