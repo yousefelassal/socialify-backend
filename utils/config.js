31fac29d1af4d5366bca0b63bbdadf5c3357e7fb
Yousef <yousefsameh24@gmail.com>
2023-09-07 20:24:38 +0300

    fix type

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const MOGODB_URI = process.env.NODE_ENV === 'test'
-? process.env.TEST_MOGODB_URI
-: process.env.MOGODB_URI;
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+? process.env.TEST_MONGODB_URI
+: process.env.MONGODB_URI;
 
 module.exports = {
-    MOGODB_URI
+    MONGODB_URI
 }
