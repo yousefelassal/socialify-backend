349a890fb7f544aed91485144018f8fea52682ab
Yousef <yousefsameh24@gmail.com>
2023-09-07 20:24:46 +0300

    connect to db

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require('express')
+require('express-async-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const config = require('./utils/config');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+mongoose.connect(config.MONGODB_URI).then(() => {
+    console.log('connected to MongoDB');
+}).catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+});
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
