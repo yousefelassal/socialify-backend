f1c6225d7b0b7c0f1bc7810f5fd078faa6762ec1
Yousef <yousefsameh24@gmail.com>
2023-09-09 00:36:13 +0300

    return profile pic

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -28,7 +28,7 @@ loginRouter.post('/', async (req, res) => {
         { expiresIn: 60 * 60 }
     );
 
-    res.status(200).send({ token, username: user.username, name: user.name });
+    res.status(200).send({ token, username: user.username, name: user.name, profile_pic: user.profile_pic });
 });
 
 module.exports = loginRouter;
