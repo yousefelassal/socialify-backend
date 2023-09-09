aad28ec8c5ff706f281f9932748c7b11b948ae08
Yousef <yousefsameh24@gmail.com>
2023-09-09 21:42:14 +0300

    create search controller

---
+++ controllers/searchController.js
@@ -0,0 +1,22 @@
+const searchRouter = require('express').Router();
+const User = require('../models/user');
+const Post = require('../models/post');
+
+searchRouter.get('/', async (req, res) => {
+    const { query } = req.query;
+    const users = await User.find({ username: { $regex: query, $options: 'i' } }).select('username name profile_pic');
+    const posts = await Post.find({ content: { $regex: query, $options: 'i' } }).populate('user', 'username name profile_pic');
+    res.json({ users, posts });
+});
+
+searchRouter.get('/users', async (req, res) => {
+    const users = await User.find({}).select('username name profile_pic');
+    res.json(users);
+});
+
+searchRouter.get('/posts', async (req, res) => {
+    const posts = await Post.find({}).populate('user', 'username name profile_pic');
+    res.json(posts);
+});
+
+module.exports = searchRouter;
