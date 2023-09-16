6eb6d059e2e8a3b2e14f397993294e3040c80666
Yousef <yousefsameh24@gmail.com>
2023-09-16 15:09:03 +0300

    add likes controller

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const postController = require('../controllers/postController');
+const likesController = require('../controllers/likesController');
 const auth = require('../middleware/auth');
 
 router.get('/', postController.index);
@@ -9,6 +10,10 @@ router.get('/', postController.index);
 router.post('/', postController.create);
 
 router.put('/:id', postController.update);
+
+router.post('/:id/like', likesController.like);
+
+router.post('/:id/unlike', likesController.unlike);
 
 router.delete('/:id', postController.delete);
 
