3f1989c70654e548755bdc669840ac6faa794b99
Yousef <yousefsameh24@gmail.com>
2023-09-18 14:35:49 +0300

    use comment controller

--- routes/posts.js
+++ routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const postController = require('../controllers/postController');
 const likesController = require('../controllers/likesController');
+const commentController = require('../controllers/commentController');
 const auth = require('../middleware/auth');
 
 router.get('/', postController.index);
@@ -10,6 +11,12 @@ router.get('/', postController.index);
 router.post('/', postController.create);
 
 router.put('/:id', postController.update);
+
+router.post('/:id/comment', commentController.create);
+
+router.put('/:id/comment/:commentId', commentController.update);
+
+router.delete('/:id/comment/:commentId', commentController.delete);
 
 router.post('/:id/like', likesController.like);
 
