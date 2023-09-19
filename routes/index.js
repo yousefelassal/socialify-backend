a0bc282cff42ef387a949f11b3644e0e44fdbd35
Yousef <yousefsameh24@gmail.com>
2023-09-19 11:06:38 +0300

    add todo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//TODO: add sample ids to paths
+
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'API Endpoints',
